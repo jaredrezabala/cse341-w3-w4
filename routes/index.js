b131693b8ce626a68ced42cfd02346389bf51960
jaredrezabala <rezabalajared@gmail.com>
2024-10-09 20:33:18 -0500

    fixed some routes and adjusted Swagger

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,36 @@
 const express = require('express')
 const router = express.Router()
+const session = require('express-session')
+const passport = require('passport')
 
 router.get("/", (req, res) => {
     //#swagger.tags=['Hello World']
-    res.send('Welcome to my web!')
+    res.send(req.session.profile !== undefined ? `Welcome to my API ${req.session.profile.username} visit /logout to sign out!` : `Welcome to my API, please visit /login to get full access to the endpoints!`)
+    console.log(req.session.profile)
 })
 
 router.use("/movies", require('./movies'))
-router.use("/auth", require("./auth"))
+//#swagger.ignore = true
+router.get('/login', passport.authenticate('github', { 
+    scope: ['user:email'] })//#swagger.ignore = true
+);
+
+router.get('/github/callback',
+    passport.authenticate('github', { failureRedirect: '/' }),
+    (req, res) => {
+        req.session.profile  = req.user;
+        res.redirect('/api-docs'); 
+    }
+//#swagger.ignore = true
+);
+router.get('/logout', (req, res) => {
+    req.logout((err) => {
+        if(err) {
+            return res.status(400).json({ message: err.message });
+        }else{
+            res.redirect('/')
+        }
+    });
+}//#swagger.ignore = true
+)
 module.exports = router
